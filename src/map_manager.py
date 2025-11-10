88dfe6719002bd6cd79e0873d9838622d86a9229
eszpimporte <eszpimporte@gmail.com>
2025-11-10 23:58:22 +0100

    connerie n°1bisbisbis l'empire du NCI ne contre attaque pas

--- src/map_manager.py
+++ src/map_manager.py
@@ -7,14 +7,14 @@ class Map_Manager():
 
 
         #Load les données -> à exporter
-        self.tmx_data = pytmx.util_pygame.load_pygame("img/assets/cartes/map_rouilny_0_0.tmx")
+        self.tmx_data = pytmx.util_pygame.load_pygame("img/assets/cartes/rouilny/map_rouilny_0_0.tmx")
         
         #Récupérer les données pour pyscroll
         self.map_data = pyscroll.data.TiledMapData(self.tmx_data)
         
         #Les calques
         self.map_layer = pyscroll.orthographic.BufferedRenderer(self.map_data, self.screen.get_size())
-        self.map_layer.zoom = 1.5
+        self.map_layer.zoom = 2
 
 
     def switch_map(self):
--- src/player.py
+++ src/player.py
@@ -9,3 +9,7 @@ class Player(Sprite):
         super().__init__((320,100),'img/assets/sprites/personnage_base.png')
         self.movible = True
         self.speed = Player.DEFAULT_PLAYER_SPEED
+
+        #zone et tronçon où se situe le player
+        self.zone = "rouilny"
+        self.troncon = (0,0)
